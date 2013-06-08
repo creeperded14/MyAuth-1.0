d5b8830afd7162ccea1c26d080668fae25c14171
creeperded14 <creeperdedpt@gmail.com>
2013-06-08 13:23:39 +0200

    Update PasswordSecurity.java

--- src/uk/org/whoami/authme/security/PasswordSecurity.java
+++ src/uk/org/whoami/authme/security/PasswordSecurity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Sebastian Köhler <sebkoehler@whoami.org.uk>.
+ * Copyright 2013 creeperde14 [creeperdedpt@gmail.com]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package uk.org.whoami.authme.security;
+package uk.org.creeperded14.myauth.security;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
